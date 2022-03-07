0a75ac0dcb9552143c823d6efc192406e2f3a2ac
Alimi Rereloluwa Victor <71636617+rereloluwavictor2001@users.noreply.github.com>
2022-03-07 06:12:42 +0100

    Update GPAcal.py
    Basically, I just tried accounting for possible errors on the part of the user so instead of the code sopping, it just alerts the user to correct himself. But then, I could only account for just a chance for a trial. If similar error occur again, which has a lesser chance of happening, the traceback error displays again. There's still more improvement though. Happy learning Tife

--- GPAcal.py
+++ GPAcal.py
@@ -2,7 +2,7 @@
     This program recieves the courses, course units, scores and calculates the GPA of any given number of students
     It works without having any function, kindly check it out
 '''
-
+import sys
 Grading = {"A": 70-100, "B": 60-69, "C": 50-59, "D": 45-49, "E": 40-44, "F": 0-39}
 Grade= {"A": 5, "B": 4, "C": 3, "D": 2, "E": 1, "F": 0}
 
@@ -10,24 +10,44 @@ Grade= {"A": 5, "B": 4, "C": 3, "D": 2, "E": 1, "F": 0}
 Courses = []
 Units =[]
 Scores=[]
+try:
+    numberOfStudents = int(input("Hello, how many students GPA do you want to calculate: "))
+except ValueError:
+    print("Value must be a number!")
+    numberOfStudents = int(input("Hello, how many students GPA do you want to calculate: "))
 
-numberOfStudents = int(input("Hello, how many students GPA do you want to calculate: "))
 num=0
-while num< numberOfStudents:
+while num < numberOfStudents:
     name = input("Name of student: ")
-    okay =0
-    numberOfCourses= int(input("How many courses did you register for(between 5 and 10): "))
+    okay =0 
+    try:
+        numberOfCourses= int(input("How many courses did you register for(between 5 and 10): "))
+    except ValueError:
+        print("Please, Enter a number between 5 and 10")
+        numberOfCourses= int(input("How many courses did you register for(between 5 and 10): "))
     if numberOfCourses == 0 or numberOfCourses<5: 
         print('You have to register for 5 or more courses')
+        continue
     elif numberOfCourses > 10:
         print('You can\'t register for more than 10 courses')
+        continue
     else:
         while okay < numberOfCourses:
-            courseCode = input("Enter your course code and the unit separated by column (Course code: unit)==> ")
-            Courses.append( courseCode.split(":")[0].upper() )
-            Units.append(int(courseCode.split(":")[1]))
-
-            score = int(input("Enter your score: "))
+            try:
+                courseCode = input("Enter your course code and the unit separated by column (Course code: unit)==> ")
+                Courses.append(courseCode.split(":")[0].upper())
+                Units.append(int(courseCode.split(":")[1]))
+            except :
+                print("Oops!", sys.exc_info()[2], "occurred.")
+                print("Check the entry format and try again!")
+                courseCode = input("Enter your course code and the unit separated by column (Course code: unit)==> ")
+                Courses.append(courseCode.split(":")[0].upper())
+                Units.append(int(courseCode.split(":")[1]))
+            try:
+                score = int(input("Enter your score: "))
+            except ValueError:
+                print("Score must be a number!")
+                score = int(input("Enter your score: "))
             Scores.append(score)
             okay +=1
 
@@ -39,7 +59,7 @@ while num< numberOfStudents:
         Grades= []
         for x in range(len(Scores)):
             if Scores[x] == 0 or Scores[x] <=39:
-                creditPoint= Units[x]*0
+                creditPoint = Units[x]*0
                 creditPoints.append(creditPoint)
                 Grades.append("F")
 
@@ -70,9 +90,9 @@ while num< numberOfStudents:
 
         
     GPA =  sum(creditPoints)/TotalWorkload    
-    print(f"{name.upper()} is offering {Courses} \nScored the folowing {Scores} \nGrades are {Grades} respectively")
+    print(f"{name.upper()} is offering {Courses} \nScored the following {Scores} \nGrades are {Grades} respectively")
     print("===============================================================")
-    print(f"Total Credit Point(TCP) : {sum(creditPoints)} \nTotal Workload(TWL) : {TotalWorkload} \nGPA : {round(GPA,2)} \n")  
+    print(f"Total Credit Point(TCP) : {sum(creditPoints)} \nTotal Workload(TWL) : {TotalWorkload} \nGPA : {round(GPA,3)} \n")  
     
     Courses.clear() 
     Units.clear()
